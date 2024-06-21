560a330f543a1a3a1e2b2082114e4dc3b221449c
Marco <marco.matteo@edu.tbz.ch>
2024-06-21 10:19:53 +0200

    https im Backend mit SSL

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'node:fs';
+import * as process from 'node:process';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./cert/server.key'),
+    cert: fs.readFileSync('./cert/server.crt'),
+    passphrase: process.env.HTTPS_KEY_PASSPHRASE,
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   await app.listen(8000);
 }
-bootstrap();
+bootstrap().then(() => {
+  console.log('Server started on https://localhost:8000');
+});
